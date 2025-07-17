696fec9e7e7b75ca21083081dc08bc1a0de8682c
AmruthaVarshini06 <karriamruthavarshini6@gmail.com>
2025-07-17 22:57:43 +0530

    BinarySearchTree.cpp

---
+++ BinarySearchTree.cpp
@@ -0,0 +1,9 @@
+#include "BinarySearchTree.h"
+int main() {
+    BinarySearchTree BST;  // Creates a binary search tree
+    BST.Insert(10);  //Insert an element 10 into the tree
+    BST.Insert(20);  //Insert an element 20 into the tree
+    TreeNode *root;
+    BinarySearchTree BST1;
+    BST.ADD(20,root);
+}
